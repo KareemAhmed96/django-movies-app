cfc0c6727aeaeb4c3499b91b54f5ef984b26f32a
kareemzak <kareemahmed96@outlook.com>
2021-03-31 01:20:42 +0200

    changed default admin (view site) link

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Movie, Category, Rate, Country
 
+admin.site.site_url = 'http://localhost:8000/movies/'
+
 # Register your models here.
-admin.site.register(Movie, name="Movie")
+admin.site.register(Movie)
 admin.site.register(Category)
 admin.site.register(Rate)
 admin.site.register(Country)
