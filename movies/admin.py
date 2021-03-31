db6ed1555459f55a00a198c915454f27b3c2e2ed
kareemzak <kareemahmed96@outlook.com>
2021-03-31 14:24:14 +0200

    admin panel customization

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-from .models import Movie, Category, Rate, Country
+from .models import Movie, Category, Cast, Rate, Country
 
 admin.site.site_url = 'http://localhost:8000/movies/'
 
+class MovieInline(admin.StackedInline):
+    model = Movie
+    extra = 1
+    max_num = 5
+
+
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [MovieInline]
+
+
+class MoviesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'year')
+    list_filter = ('year',)
+
+
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MoviesAdmin)
 admin.site.register(Category)
 admin.site.register(Rate)
-admin.site.register(Country)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Cast)
